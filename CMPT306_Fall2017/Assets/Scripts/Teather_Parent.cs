b0b33805c6225de58a2f6405c1154de4b6284833
Daniel McCoshen <dam479@mail.usask.ca>
2017-09-25 07:11:33 -0600

    some basic attempts at the teather scripting

---
+++ CMPT306_Fall2017/Assets/Scripts/Teather_Parent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teather_Parent : MonoBehaviour {
+
+    public int health { get; private set; }
+    public int startingHealth;
+
+	// Use this for initialization
+	void Start () {
+        health = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   /// <summary>
+   /// handles recieved damage message and applies damage to the teather
+   /// </summary>
+   /// <param name="damage"> the amount of damage to apply </param>
+    void damage(int damage)
+    {
+        health -= damage;
+    }
+}
---
+++ CMPT306_Fall2017/Assets/Scripts/basicMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicMovement : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 15.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 15f;
+
+        transform.Translate(x, z, 0);
+        Vector3 MouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        transform.LookAt(MouseWorldPosition);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, transform.rotation.eulerAngles.z));
+    }
+}
