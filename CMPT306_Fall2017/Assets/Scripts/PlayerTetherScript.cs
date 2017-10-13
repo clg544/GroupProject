c0c107bacc4ea456054b4f4c9c4d13020f823dbc
clg544 <clinton.galbraith@gmail.com>
2017-10-12 21:52:10 -0600

    Added non-trivial tether physics, and a hard reset whenever line goes from taught -> non-taught

--- CMPT306_Fall2017/Assets/Scripts/PlayerTetherScript.cs
+++ CMPT306_Fall2017/Assets/Scripts/PlayerTetherScript.cs
@@ -7,6 +7,9 @@ public class PlayerTetherScript : MonoBehaviour {
     /* How far the players can go before the tether pulls them in */
     [SerializeField]
     private float engageDistance;
+    [SerializeField]
+    private float distanceMultiplier;
+    private bool taught;
 
     /* Hooke's law labels this k, how much force the tether applies */
     [SerializeField]
@@ -26,9 +29,9 @@ public class PlayerTetherScript : MonoBehaviour {
     private Rigidbody2D playerOneBody;
     private Rigidbody2D playerTwoBody;
 
-
     /* Resource to represent the tether as a line */
-    LineRenderer tetherVisual;
+    private GameObject[] tetherLinks;
+    private LineRenderer tetherVisual;
 
     /* Draw the tether as a line between the two players */
     public void DrawTetherAsLine()
@@ -48,12 +51,26 @@ public class PlayerTetherScript : MonoBehaviour {
         playerOneBody = playerOne.GetComponent<Rigidbody2D>();
         playerTwoBody = playerTwo.GetComponent<Rigidbody2D>();
 
+        tetherLinks = GameObject.FindGameObjectsWithTag("TetherLink");
+        GameObject[] temp = new GameObject[tetherLinks.Length];
+
+        for(int i = 0; i < tetherLinks.Length; i++)
+        {
+            // Sorted by GameObject Name!  Important, as we adust positions with this array.
+            Debug.Log(tetherLinks[i].name + ":" + int.Parse(tetherLinks[i].name[4].ToString() + tetherLinks[i].name[5].ToString()));
+
+            int cur = int.Parse(tetherLinks[i].name[4].ToString() + tetherLinks[i].name[5].ToString());
+            temp[cur] = tetherLinks[i];
+        }
+        tetherLinks = temp;
+
         tetherVisual = this.GetComponent<LineRenderer>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        bool curTaught = (curDistance > engageDistance);
+        
         // Get current distance
         Vector3 posOne = playerOne.transform.position;
         Vector3 posTwo = playerTwo.transform.position;
@@ -64,14 +81,14 @@ public class PlayerTetherScript : MonoBehaviour {
         curDistance = Vector3.Distance(posOne, posTwo);
         
         /* if The players are far enough apart... */
-        if (curDistance > engageDistance)
+        if (curTaught)
         {
             float distance = curDistance - engageDistance;
             /* Apply force of Distance squared, Scaled by elasticity, and divide among both players 
              *
              * I used distance squared to make the tether more responsive, though less realistic. - Clint
              */
-            float tension = (elasticity * (distance * distance)) / 2;
+            float tension = (elasticity * (distance * distanceMultiplier)) / 2;
 
             /* Vector difference gets magnitide = tension */
             difference = Vector3.ClampMagnitude(difference, tension);
@@ -81,8 +98,31 @@ public class PlayerTetherScript : MonoBehaviour {
             playerTwoBody.AddForce(difference);
         }
 
+        // if we are re-entering engage distance
+        if(taught && (!curTaught))
+        {
+            Vector3 diff = (playerTwo.transform.position - playerOne.transform.position) / (tetherLinks.Length + 1);
+
+            int i = 1;
+            foreach(GameObject link in tetherLinks)
+            {
+                link.transform.position = playerOne.transform.position + (diff * i);
+                i++;
+            }
+        }
+
         /* Draw the tether */
-        DrawTetherAsLine();
+        if (curTaught)
+        {
+            DrawTetherAsLine();
+        }
+        else
+        {
+            BroadcastMessage("ConnectNodes");
+            playerTwo.SendMessage("ConnectNodes");
+        }
+
+        taught = curTaught;
 	}
 
 }
---
+++ CMPT306_Fall2017/Assets/Scripts/TetherLinks.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetherLinks : MonoBehaviour {
+
+    public GameObject myConnection;
+    private LineRenderer tetherVisual;
+
+    public void ConnectNodes()
+    {
+        tetherVisual.SetPosition(0, this.transform.position);
+        tetherVisual.SetPosition(1, myConnection.transform.position);
+    }
+
+	// Use this for initialization
+	void Start () {
+        tetherVisual = this.GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
