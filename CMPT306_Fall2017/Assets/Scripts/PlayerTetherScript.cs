7ab32cae79136e07aa96eb144bad02b96c1c6148
clg544 <clinton.galbraith@gmail.com>
2017-10-13 22:38:06 -0600

    Added a node freezing monitor to the parent tether, which removed a lot of the jankiness to visible tether.

--- CMPT306_Fall2017/Assets/Scripts/PlayerTetherScript.cs
+++ CMPT306_Fall2017/Assets/Scripts/PlayerTetherScript.cs
@@ -6,49 +6,43 @@ public class PlayerTetherScript : MonoBehaviour {
     
     /* How far the players can go before the tether pulls them in */
     [SerializeField]
-    private float engageDistance;
+    private float engageDistance;       // Distance where players get pulled in
     [SerializeField]
-    private float distanceMultiplier;
-    private bool taught;
+    private float distanceMultiplier;   // Scaler to pull players in by
 
     /* Hooke's law labels this k, how much force the tether applies */
     [SerializeField]
-    private float elasticity;
+    private float elasticity;           // Constant tether force, scaled
 
     /* The current distance between the two players */
-    [SerializeField]
     private float curDistance;
     
     /* Our Players */
     [SerializeField]
-    private GameObject playerOne;
+    private GameObject playerOne;       
     [SerializeField]
     private GameObject playerTwo;
-    
     private Rigidbody2D playerOneBody;
     private Rigidbody2D playerTwoBody;
 
     /* Resource to represent the tether as a line */
-    private GameObject[] tetherLinks;
-    public int tetherResolution;
-    private LineRenderer tetherVisual;
+    private GameObject[] tetherLinks;   // All the tether's child links
+    private TetherLinks[] tetherScripts;// Scripts for the tethers
+    public int tetherResolution;        // How many nodes to use
     [SerializeField]
-    private float linkSpeed;
-    [SerializeField]
-    private GameObject linkNode;
+    private GameObject linkNode;        // Link prefab to clone
 
-    /* Draw the tether as a line between the two players */
-    public void DrawTetherAsLine()
-    {
-        tetherVisual.SetPosition(0, playerOne.transform.position);
-        tetherVisual.SetPosition(1, playerTwo.transform.position);
-    }
+    public float freezeDistance;        // Child's frozen distance
+    private bool frozen;                // a pair of nodes passed a distance threshold
+    private GameObject whoFroze;        // the node who froze the tether 
 
+    /* Get the centre of the players */
     public Vector3 getCentre()
     {
         return (playerOne.transform.position + playerTwo.transform.position) / 2;
     }
 
+    /* Distribute each node in tetherLinks[] in an equidistant distribution */
     public void distributeNodes()
     {
         Vector3 diff = (playerTwo.transform.position - playerOne.transform.position) / (tetherLinks.Length + 1);
@@ -57,25 +51,52 @@ public class PlayerTetherScript : MonoBehaviour {
         foreach (GameObject link in tetherLinks)
         {
             link.transform.position = playerOne.transform.position + (diff * i);
-            //link.GetComponent<Rigidbody2D>().velocity = Vector3.MoveTowards(link.transform.position, playerOne.transform.position + (diff * i), linkSpeed);
             i++;
         }
     }
 
-    // Use this for initialization
+    /* A node hit something pointy, so freeze offending node untin they figure it out */
+    public void Freeze(GameObject freezer)
+    {
+        if (!(frozen))
+        {
+            Rigidbody2D offenderBody = freezer.GetComponent<Rigidbody2D>();
+
+            frozen = true;
+            whoFroze = freezer;
+
+            offenderBody.constraints = RigidbodyConstraints2D.FreezePosition;
+        }
+    }
+    public void Unfreeze()
+    {
+        if (frozen)
+        {
+            whoFroze.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+
+            frozen = false;
+            whoFroze = null;
+
+        }
+    }
+
+    /* Set up via Awake to prepare for child nodes */
     void Awake () {
+        /* Prepare players */
         playerOneBody = playerOne.GetComponent<Rigidbody2D>();
         playerTwoBody = playerTwo.GetComponent<Rigidbody2D>();
 
+        /* Create & populate child link nodes */
         tetherLinks = new GameObject[tetherResolution];
-
+        tetherScripts = new TetherLinks[tetherResolution];
         for(int i = 0; i < tetherResolution; i++)
         {
             tetherLinks[i] = Instantiate(linkNode, gameObject.transform);
+            tetherScripts[i] = tetherLinks[i].GetComponent<TetherLinks>();
         }
         distributeNodes();
 
-        /* Set the links targets, with special cases for the end links */
+        /* Set the links targets to prev node, with special cases for the end links */
         tetherLinks[0].GetComponent<DistanceJoint2D>().connectedBody = playerOneBody;
         tetherLinks[0].GetComponent<TetherLinks>().setConnection(playerOne);
         for (int i = 1; i < tetherLinks.Length; i++)
@@ -85,31 +106,41 @@ public class PlayerTetherScript : MonoBehaviour {
         }
         playerTwo.GetComponent<DistanceJoint2D>().connectedBody = tetherLinks[tetherLinks.Length - 1].GetComponent<Rigidbody2D>();
         playerTwo.GetComponent<TetherLinks>().setConnection(tetherLinks[tetherLinks.Length - 1]);
-        
-        tetherVisual = this.GetComponent<LineRenderer>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        bool curTaught = (curDistance > engageDistance);
-        
-        // Get current distance
+        // Get current distance between players
         Vector3 posOne = playerOne.transform.position;
         Vector3 posTwo = playerTwo.transform.position;
 
-        // Calculate vector of p1 to p2
-        Vector3 difference = posOne - posTwo;
-        // curdistance = Magnitide of difference
-        curDistance = Vector3.Distance(posOne, posTwo);
+        Vector3 difference;
 
-        /* if The players are far enough apart... */
-        if (curTaught)
+        if (!(frozen))
+        {
+            // Calculate vector of p1 to p2
+            difference = posOne - posTwo;
+
+            // curdistance = Magnitide of difference
+            curDistance = Vector3.Distance(posOne, posTwo);
+        }
+        else
+        {
+            Vector3 posFrz = whoFroze.transform.position;
+
+            // Calculate vector of p1 to p2
+            difference = posOne - posTwo - posFrz;
+
+            // curdistance = Magnitide of difference
+            curDistance = Vector3.Distance(posOne, posFrz) + Vector3.Distance(posTwo, posFrz);
+        }
+
+        /* if The players are too far apart... */
+        if (curDistance > engageDistance)
         {
             float distance = curDistance - engageDistance;
-            /* Apply force of Distance squared, Scaled by elasticity, and divide among both players 
-             *
-             * I used distance squared to make the tether more responsive, though less realistic. - Clint
-             */
+
+            /* Apply force of Distance squared, Scaled by elasticity, and divide among both players */
             float tension = (elasticity * (distance * distanceMultiplier)) / 2;
 
             /* Vector difference gets magnitide = tension */
@@ -120,10 +151,26 @@ public class PlayerTetherScript : MonoBehaviour {
             playerTwoBody.AddForce(difference);
         }
 
+        /* Tell all nodes to render a line */ 
         BroadcastMessage("ConnectNodes");
         playerTwo.SendMessage("ConnectNodes");
 
-        taught = curTaught;
+        int overDist = 0;
+        if (frozen)
+        {
+            foreach(TetherLinks tl in tetherScripts)
+            {
+                if(tl.getDistance() > tl.freezeDistance)
+                {
+                    overDist++;
+                }
+            }
+
+            if(overDist <= 2)
+            {
+                Unfreeze();
+            }
+        }
 	}
 
 }
--- CMPT306_Fall2017/Assets/Scripts/TetherLinks.cs
+++ CMPT306_Fall2017/Assets/Scripts/TetherLinks.cs
@@ -4,25 +4,48 @@ using UnityEngine;
 
 public class TetherLinks : MonoBehaviour {
 
-    public GameObject myConnection;
-    private Rigidbody2D myBody;
-    private LineRenderer tetherVisual;
+    public PlayerTetherScript tetherManager;    // The parent script
 
+    public GameObject myConnection;             // What this node is connected to
+    private Rigidbody2D myBody;                 // My RigidBody
+    private Rigidbody2D connectionBody;         // Connections RigidBody
+    private LineRenderer tetherVisual;          // My lineRenderer
+
+    public float freezeDistance;
+
+    /* Set lineRenderer to draw from me to my connection */
     public void ConnectNodes()
     {
         tetherVisual.SetPosition(0, this.transform.position);
         tetherVisual.SetPosition(1, myConnection.transform.position);
     }
 
+    public float getDistance()
+    {
+        return Vector3.Distance(gameObject.transform.position, myConnection.transform.position);
+    }
+
+    void Awake()
+    {
+        tetherManager = transform.parent.gameObject.GetComponent<PlayerTetherScript>();
+    }
+
 	// Use this for initialization
-	void Start () {
+	void Start() {
         myBody = this.GetComponent<Rigidbody2D>();
         tetherVisual = this.GetComponent<LineRenderer>();
+        connectionBody = myConnection.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float curDistance = Vector3.Distance(gameObject.transform.position, myConnection.transform.position);
+
+        if(curDistance > freezeDistance)
+        {
+            if (tetherManager != null)
+                tetherManager.Freeze(gameObject);
+        }
 	}
 
     public void setConnection(GameObject gameOb)
