6492a65b12405c1cb87c49d6387de62ab9ffc0c1
clg544 <clinton.galbraith@gmail.com>
2017-11-25 19:56:53 -0600

    Added a scoring system to Players scene

---
+++ CMPT306_Fall2017/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public int curScore;
+    public Text scoreText;
+
+    public void AddToScore(int num)
+    {
+        curScore += num;
+        scoreText.text = "$" + curScore;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ CMPT306_Fall2017/Assets/Scripts/TreasureScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreasureScript : MonoBehaviour {
+    
+    public int worth;
+    public ScoreManager myScoreManager;
+    
+    public void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.transform.tag == "Player")
+        {
+            myScoreManager.AddToScore(worth);
+            Destroy(gameObject);
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
