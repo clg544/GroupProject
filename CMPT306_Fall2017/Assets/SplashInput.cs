6d2ed7d865e3bddeb553346d8a81bd0c521903c3
Brandon Piller <brandonpiller@Brandons-MacBook-Pro.local>
2017-12-06 11:38:16 -0600

    Hooked up splash screen and changed cross hair colour for fighty and shooty

--- CMPT306_Fall2017/Assets/SplashInput.cs
+++ CMPT306_Fall2017/Assets/SplashInput.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SplashInput : MonoBehaviour {
+
+	public string easyScene;
+	public string AButton;
 
 	// Use this for initialization
 	void Start () {
@@ -11,7 +15,13 @@ public class SplashInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetKeyDown (KeyCode.T)) {
+			SceneManager.LoadScene(easyScene);
+		}
+	
+		if (Input.GetKeyDown("joystick button 0")) {
+			SceneManager.LoadScene(easyScene);
+		}
 	}
-		
 }
